b714c29b27e048211e2d546ce6d03df59b95f159
sharon-launchcode <sharon.launchcode@gmail.com>
2018-03-14 22:58:16 -0400

    Test one no bad gateway on JobFieldsViewModels after extend JobFieldsViewModel to BaseModel

--- src/TechJobs/ViewModels/JobFieldsViewModel.cs
+++ src/TechJobs/ViewModels/JobFieldsViewModel.cs
@@ -8,7 +8,7 @@ namespace TechJobs.ViewModels
     {
         public JobFieldType Column { get; set; }
     }
-    public class JobFieldsViewModel
+    public class JobFieldsViewModel : BaseViewModel
     {
         // TODO #7.2 - Extract members common to SearchJobsViewModel
         // to BaseViewModel
@@ -18,7 +18,7 @@ namespace TechJobs.ViewModels
 
 
         // The current column
-        public JobFieldType Column { get; set; }
+        //public JobFieldType Column { get; set; }
 
         // All fields in the given column
         public IEnumerable<JobField> Fields { get; set; }
